1a24da1f91bb85447dcdd52ea4b65aa6c15849c9
ibrahim786in <md.ibrahim786in@gmail.com>
2018-09-21 18:40:36 +0530

    Custom Adapter With Video Play in Grid View

--- app/src/main/java/com/example/ibrahim/listviewexample/CustomListViewExample.kt
+++ app/src/main/java/com/example/ibrahim/listviewexample/CustomListViewExample.kt
@@ -7,35 +7,58 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_custom_list_view_example.*
+import kotlinx.android.synthetic.main.video_listview_design.*
 import java.io.File
 
 class CustomListViewExample : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_custom_list_view_example)
-        val permisStatus = ContextCompat.checkSelfPermission(this@CustomListViewExample, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//        setContentView(R.layout.activity_custom_list_view_example) /* This layout For Custom Adapter for Images with texts*/
+        setContentView(R.layout.video_listview_design)
+        val permisStatus = ContextCompat.checkSelfPermission(this@CustomListViewExample,Manifest.permission.READ_EXTERNAL_STORAGE)
         if (permisStatus == PackageManager.PERMISSION_GRANTED) readFile()
-        else ActivityCompat.requestPermissions(this@CustomListViewExample, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET),7007)
+        else ActivityCompat.requestPermissions(this@CustomListViewExample, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET), 7007)
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (grantResults[0]== PackageManager.PERMISSION_GRANTED) readFile()
-        else Toast.makeText(this@CustomListViewExample,"The Permission is denied", Toast.LENGTH_LONG).show()
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) readFile()
+        else Toast.makeText(this@CustomListViewExample, "The Permission is denied", Toast.LENGTH_LONG).show()
     }
 
-     fun readFile() {
-        var path = "/storage/sdcard0/WhatsApp/Media/WhatsApp Images/"
-        var file = File(path)
-        if (!file.exists()) {
-            path = "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/"
-            file = File(path)
+    fun readFile() {
+        var path1 = "/storage/sdcard0/WhatsApp/Media/WhatsApp Images/"
+        var path2 = "/storage/sdcard0/"
+        var path3 = "/storage/sdcard0/WhatsApp/Media/WhatsApp Video/"
+
+        var file1 = File(path1)
+        var file2 = File(path2)
+        var file3 = File(path3)
+
+        if (!file1.exists()) {
+            path1 = "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/"
+            file1 = File(path1)
         }
 
-        val files:Array<File> = file.listFiles() //Array String of files
-        CustomListView.adapter = MyCustomAdapter(files,this@CustomListViewExample)
+        if (!file2.exists()) {
+            path2 = "/storage/emulated/0/"
+            file2 = File(path2)
+        }
+        if (!file3.exists()) {
+            path3 = "/storage/emulated/0/WhatsApp/Media/WhatsApp Video/"
+            file3 = File(path3)
+        }
+
+        val files1: Array<File> = file1.listFiles() //Array of files
+
+        val files3: Array<File> = file3.listFiles() //Array of files
+
+        val files2: Array<String> = file2.list() //Array of String
+
+        gallery.adapter=MyCustomAdapter(files3,this) //For Video list
+
+//        listVfrmCustLay.adapter = MyCustomAdapter(files1, this@CustomListViewExample)
 
     }
 }
--- app/src/main/java/com/example/ibrahim/listviewexample/MyCustomAdapter.kt
+++ app/src/main/java/com/example/ibrahim/listviewexample/MyCustomAdapter.kt
@@ -1,39 +1,49 @@
 package com.example.ibrahim.listviewexample
 
-import android.app.AlertDialog
-import android.graphics.BitmapFactory
-import android.media.ThumbnailUtils
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import kotlinx.android.synthetic.main.custominflaterlayout.view.*
+import kotlinx.android.synthetic.main.custlistvvideo.view.*
 import java.io.File
 
-class MyCustomAdapter(var files: Array<File>, var customListViewExample: CustomListViewExample) : BaseAdapter() {
+class MyCustomAdapter(var files1: Array<File>, var customListViewExample: CustomListViewExample) : BaseAdapter() {
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
         val inflater = LayoutInflater.from(customListViewExample)
-        val viewInf = inflater.inflate(R.layout.custominflaterlayout, null)
-        val file = files[position]
+//        val viewInf = inflater.inflate(R.layout.cust_list_design_img, null) /* This layout For Custom Adapter for Images with texts*/
+        val file1 = files1[position]
 
-//        viewInf.img_preview.setImageURI(Uri.fromFile(file))  //This is to set the Original Image
+        val viewInf = inflater.inflate(R.layout.custlistvvideo, null)
 
-        val bitmap = BitmapFactory.decodeFile(file.path)
-        val img_Thumbnail = ThumbnailUtils.extractThumbnail(bitmap, 80, 80)
-        viewInf.img_preview.setImageBitmap(img_Thumbnail)
-        viewInf.file_nameTV.text = file.name
-        viewInf.file_sizeTV.text = "${file.length() / 1024} KB"
+        viewInf.videoView.setVideoURI(Uri.fromFile(file1))
+        viewInf.checkbox.text=file1.name
+        viewInf.checkbox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) viewInf.videoView.start()
+
+            else viewInf.videoView.pause()
+        }
+
+
+//        viewInf.img_preview.setImageURI(Uri.fromFile(file2))  //This is to set the Original Image
+
+        /*val bitmap = BitmapFactory.decodeFile(file1.path)
+        val imgThumbnail = ThumbnailUtils.extractThumbnail(bitmap, 60, 60)
+        viewInf.img_preview.setImageBitmap(imgThumbnail)
+        viewInf.file_nameTV.text = file1.name
+        viewInf.file_nameTV.text = file1.name
+        viewInf.file_sizeTV.text = "${file1.length() / 1024} KB"
+
         viewInf.delete_IV.setOnClickListener {
-
-            val alertDialog = AlertDialog.Builder(customListViewExample)
+        val alertDialog = AlertDialog.Builder(customListViewExample)
 
             alertDialog.setTitle("Confirm")
             alertDialog.setMessage("Are You Sure You want to delete this?")
             alertDialog.setPositiveButton("Yes") { dialog, which ->
-                file.delete()
+                file1.delete()
                 customListViewExample.readFile()
                 dialog.dismiss()
             }
@@ -42,8 +52,8 @@ class MyCustomAdapter(var files: Array<File>, var customListViewExample: CustomL
             }
             val alertDialogNew: AlertDialog = alertDialog.create()
             alertDialogNew.show()
+        }*/
 
-        }
         return viewInf
     }
 
@@ -56,6 +66,7 @@ class MyCustomAdapter(var files: Array<File>, var customListViewExample: CustomL
     }
 
     override fun getCount(): Int {
-        return files.size
+        return files1.size
+
     }
 }
